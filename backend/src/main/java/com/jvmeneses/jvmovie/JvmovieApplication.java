a79412339cdc0d9f1eec3c85a46b2eaa6c8041be
Joao <joaovic2012@hotmail.com>
2023-03-19 10:57:54 -0300

    Project Created

---
+++ backend/src/main/java/com/jvmeneses/jvmovie/JvmovieApplication.java
@@ -0,0 +1,13 @@
+package com.jvmeneses.jvmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jvmeneses/jvmovie/JvmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmeneses.jvmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
